91d42c65342c91178870b1d1f98c2e10e4c05b18
Mohammed Shalaby <mohammed.shalaby@mail.mcgill.ca>
2024-01-25 10:36:39 -0500

    Fix mistake with timestamping

--- src/utils.py
+++ src/utils.py
@@ -1,22 +1,24 @@
 import numpy as np
 
+# Speed of light
+c = 299792458 # m/s
+
 def compute_range_data(
     time: float, # in nanoseconds
-    tag1: dict, 
-    tag2: dict,
+    tag0: dict, # initiator 
+    tag1: dict, # target
     del_t_32: float, # in nanoseconds
     del_t_53: float # in nanoseconds
 ):
-    c = 299792458 # m/s
-    distance = compute_range(tag1['position'], tag2['position'])
+    distance = compute_range(tag0['position'], tag1['position'])
     tof = distance / 299792458 * 1e9 # in nanoseconds
     
-    tx1 = time + tag1['offset']
-    rx1 = time + tag2['offset'] + tof
-    tx2 = time + tag2['offset'] + tof + tag2['skew'] * del_t_32
-    rx2 = time + tag1['offset'] + 2*tof + tag1['skew'] * del_t_32
-    tx3 = time + tag2['offset'] + tof + tag2['skew'] * del_t_53
-    rx3 = time + tag1['offset'] + 2*tof + tag1['skew'] * del_t_53
+    tx1 = time + tag0['offset']
+    rx1 = time + tag1['offset'] + tof
+    tx2 = time + tag1['offset'] + tof + (1 + tag1['skew']) * del_t_32
+    rx2 = time + tag0['offset'] + 2*tof + (1 + tag0['skew']) * del_t_32
+    tx3 = time + tag1['offset'] + tof + (1 + tag1['skew']) * del_t_53
+    rx3 = time + tag0['offset'] + 2*tof + (1 + tag0['skew']) * del_t_53
     
     # Shalaby's double-sided two-way ranging protocol
     range = 0.5 * c / 1e9 * (
